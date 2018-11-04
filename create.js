0445617a909e8fba48c0ea65671fa019d38c2ecb
bscaspar <bradyscaspar@outlook.com>
2018-11-04 11:34:20 -0800

    initial checkin, backend code finished

---
+++ create.js
@@ -0,0 +1,29 @@
+import uuid from 'uuid';
+import AWS from 'aws-sdk';
+
+import * as dynamoDbLib from './libs/dynamodb-libs';
+import { success, failure } from './libs/response-lib';
+
+AWS.config.update({ region: "us-west-2" });
+
+export async function main(event, context) {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: "notes",
+        Item: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: uuid.v1(),
+            content: data.content,
+            attachment: data.attachment,
+            createdAt: Date.now()
+        }
+    };
+
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+    } catch (e) {
+        console.log(e);
+        return failure({ status: false });
+    }
+}
